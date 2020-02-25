113cd1a40553fb9a3b0f48a242d6b71842205734
DanielSolheim <danielsolheim1@gmail.com>
2020-02-25 14:52:25 +0100

    Task completed

---
+++ js/master.js
@@ -0,0 +1,48 @@
+
+//1
+$('#question1').addClass('answer');
+
+
+//2
+$('.question2').addClass('answer');
+
+
+//3
+$( '.question3 span').addClass('answer');
+
+//4
+
+$( '.question4 div').addClass('answer');
+
+//5
+$( '.question5 > div > div > div').addClass('answer');
+
+
+//6
+$( '.question6 > div > div > p,.question6 > div > p').addClass('answer');
+
+//7
+$( '.question7 > div > div > p, .question7 > div > a').addClass('answer');
+
+//8
+$( '.question8 > ul > li:first-child').addClass('answer');
+
+//9
+$( '.question9 > ul > li:last-child').addClass('answer');
+
+//10
+$('.question10 > ul > li:last-child > a ').addClass('answer');
+
+
+//11
+$('.question11 > ul > li > a[href] ').addClass('answer');
+
+
+
+//12
+$('.question12 > ul > li > a[href*="http:"] ').addClass('answer');
+
+
+//13
+
+$('.question13 > ul > li > a[href!="#"] ').addClass('answer');
